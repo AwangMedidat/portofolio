2d8bb404c4ff343e47ac366fdc5c25bd69003e42
AwangMedidat <awangmedidat98@gmail.com>
2024-02-08 02:03:25 +0700

    feat: Add Header Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Profie from '../src/assets/Photo.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className='container m-auto px-4 py-6'>
+          <div className="flex justify-between items-center">
+            <div>
+              <h1 className='font-bold text-xl'>Awang Portofolio</h1>
+            </div>
+            <div>
+              <ul className='flex gap-4'>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>About Me</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section>
+          <div className="container m-auto px-4 py-10 flex gap-6">
+            <div>
+              <h2 className='font-bold text-3xl'>Hello, I'm Awang</h2>
+              <h2>frontend developer</h2>
+              <p>Frontend Developer, seeking to apply competent development skills with a focus on collaboration, communication, and expertise.</p>
+            </div>
+            <div>
+              <img src={Profie} width={220} />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
